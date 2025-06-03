0c9c9e44e772cc04619b74cc34cf8b94eed544a8
zxcaran <131495591+zxcaran@users.noreply.github.com>
2025-06-04 00:14:02 +0300

    Add files via upload

---
+++ crash/script1.js
@@ -0,0 +1,82 @@
+let lastBettingTime = 0; 
+let tokenIndex = 0;
+
+const tokens = [
+    "demo",
+    "demo",
+    "demo"
+];
+
+function getAuthorizationToken() {
+    const token = tokens[tokenIndex];
+    tokenIndex = (tokenIndex + 1) % tokens.length;
+    return `Bearer ${token}`;
+}
+
+function getRan(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+async function checkSignal() {
+    let randomNumber1 = getRan(1.1, 5.0).toFixed(2);
+    const url = 'https://crash-gateway-cc-cr.gamedev-tech.cc/state?id_n=1play_fastcrash&id_i=1';
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': getAuthorizationToken()
+        }
+    });
+    const data = await response.json();
+    const state = data.current_state;
+
+
+    let responseText = document.getElementById('responseText');
+    if (!responseText) {
+        console.error('Element with ID responseText not found.');
+        return;
+    }
+
+    if (state === "betting" && Date.now() - lastBettingTime > 5000) {
+        let resultText = `${randomNumber1}x`;
+        document.getElementById("responseText").textContent = resultText;
+        localStorage.setItem('resultText', resultText);
+        responseText.className = 'text betting';        
+        lastBettingTime = Date.now();
+    } else if (state === "ending") {
+        responseText.textContent = "Waiting..";
+        responseText.className = 'text fly';
+    } 
+}
+
+function fetchDataAndUpdate() {
+    const url = 'https://crash-gateway-cr.100hp.app/state?id_n=1play_fastcrash';
+    fetch(url, {
+        headers: {
+            'Authorization': getAuthorizationToken()
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const kef = parseFloat(data.current_coefficients);
+            updateCoefficients(kef);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function updateCoefficients(coefficients) {
+    const coefficientsDiv = document.getElementById('coefficients');
+    if (!coefficientsDiv) {
+        console.error('Element with ID coefficients not found.');
+        return;
+    }
+
+    if (coefficients !== 1) {
+        coefficientsDiv.innerText = `x${coefficients}`; 
+        coefficientsDiv.classList.remove('smallt');
+        coefficientsDiv.classList.add('kif');
+    } 
+}
+
+fetchDataAndUpdate();
+setInterval(fetchDataAndUpdate, 100);
+let intervalId = setInterval(checkSignal, 100);
+checkSignal();
